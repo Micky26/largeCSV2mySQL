79f534ef5cde93ebbebd15f808bf845f7f40a13f
Michael Mgbame <40023569+Micky26@users.noreply.github.com>
2020-07-20 23:53:11 +0100

    Update csv2sql.php

--- csv2sql.php
+++ csv2sql.php
@@ -86,13 +86,13 @@ $count1=(int)$r1['count'];
 //If each tuple in CSV are not seperated by new line.  replace \n in the below query  the delimiting character which seperates two tuples in csv
 // for more information about the query http://dev.mysql.com/doc/refman/5.1/en/load-data.html
 mysqli_query($cons, '
-    LOAD DATA LOCAL INFILE "'.$file.'"
-        INTO TABLE '.$table.'
+    LOAD DATA LOCAL INFILE "HelpNow"
+        INTO TABLE 'HelpNow'
         FIELDS TERMINATED by \',\'
         LINES TERMINATED BY \'\n\'
 ')or die(mysql_error());
 
-$result2=mysqli_query($cons,"select count(*) count from $table");
+$result2=mysqli_query($cons,"select count(*) count from HelpNow");
 $r2=mysqli_fetch_array($result2);
 $count2=(int)$r2['count'];
 
